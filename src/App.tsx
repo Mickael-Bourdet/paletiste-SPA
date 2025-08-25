7cbfa40bc25c432f4aa60c5c1e88093d1cf1bb34
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-08-25 15:25:56 +0200

    feat : component to go to the top of the page each time page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import BackToTopPage from "./components/BackToTopPage";
 
 function App() {
-  return <></>;
+  return (
+    // Wrapper for the entire app
+    <div className="bg-body min-h-screen text-primary flex flex-col">
+      <BackToTopPage />
+    </div>
+  );
 }
 
 export default App;
