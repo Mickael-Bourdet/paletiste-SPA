2a721049e59266ac9031ceba676e317fd95cffa9
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-08-28 11:32:33 +0200

    feat : header for medium & large screen

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,20 +3,32 @@ import { useThemeMode } from "../../utils/useThemeMode";
 export default function Header() {
   const { theme } = useThemeMode();
   return (
-    <div className="py-4 px-10">
-      <div className="flex justify-center items-center mx-auto">
+    <header className="py-4 px-8 border-b border-gray-300 dark:border-gray-700 md:px-16 xl:px-48 md:flex items-center">
+      <div className="flex justify-center items-center md:justify-start">
         <img
           src={
             theme === "dark"
               ? "/img/logo/logo_paletiste_blanc.webp"
               : "/img/logo/logo_paletiste_noir.webp"
           }
-          width={64}
-          height={64}
-          alt=""
+          className="w-16 h-16 md:w-12 md:h-12"
+          alt="Logo du site paletiste représentant deux joueurs se serrant la main autour d'une planche"
         />
-        <p className="text-4xl font-title ml-2">Paletiste</p>
+        <p className="text-4xl font-title ml-2 md:text-2xl">Paletiste</p>
       </div>
-    </div>
+      <nav className="hidden md:flex flex-1 justify-center">
+        <p>lien 1</p>
+        <p>lien 2</p>
+        <p>lien 3</p>
+      </nav>
+      <div className="hidden md:flex items-baseline gap-4">
+        <i className="fa-solid fa-magnifying-glass border p-2 border-gray-300 bg-gray-300"></i>
+        <i className="fa-solid fa-user border p-2 border-gray-300 bg-gray-300"></i>
+        <button className="rounded-full py-1 px-4 bg-royal text-body text-sm translate-y-1 ">
+          <i className="fa-solid fa-plus"></i>
+          <span className="pl-2 text-lg">Créer</span>
+        </button>
+      </div>
+    </header>
   );
 }
