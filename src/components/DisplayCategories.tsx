ca742864735046a7de055182dfa3527404eea2e0
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-09-10 10:47:56 +0200

    feat : add margin and color text in dark mode

--- src/components/DisplayCategories.tsx
+++ src/components/DisplayCategories.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function DisplayCategories() {
   return (
     <div className="wrapper py-10">
-      <h2 className="text-3xl mb-4 font-title md:text-left md:pl-6 mdl:pl-14 xlg:pl-10 2xl:pl-0">
+      <h2 className="text-3xl mb-6 font-title md:text-left md:pl-6 mdl:pl-14 xlg:pl-10 2xl:pl-0">
         Les concours par catégories
       </h2>
       <ul className="wrapper flex justify-between items-baseline flex-wrap gap-6">
         <li className="relative max-w-xs w-90 mx-auto">
-          <Link to="/categories/fonte">
+          <Link to="/events?category=fonte">
             <div className="h-50 relative">
               <img
                 src={`/img/categories/fonte.jpg`}
@@ -17,11 +17,11 @@ export default function DisplayCategories() {
                 className="rounded-xl brightness-50 w-full h-full object-cover absolute inset-0"
               />
               <div className="absolute inset-0 flex items-center justify-center flex-col gap-10">
-                <p className="text-body font-extrabold text-3xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-3xl text-center drop-shadow-lg px-2">
                   {" "}
                   Fonte
                 </p>
-                <p className="text-body font-extrabold text-xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-xl text-center drop-shadow-lg px-2">
                   {" "}
                   406 concours
                 </p>
@@ -39,11 +39,11 @@ export default function DisplayCategories() {
                 className="rounded-2xl brightness-50 w-full h-full object-cover absolute inset-0"
               />
               <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center">
-                <p className="text-white font-extrabold text-3xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-3xl text-center drop-shadow-lg px-2">
                   {" "}
                   Laiton
                 </p>
-                <p className="text-body font-extrabold text-xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-xl text-center drop-shadow-lg px-2">
                   {" "}
                   31 concours
                 </p>
@@ -61,11 +61,11 @@ export default function DisplayCategories() {
                 className="rounded-2xl brightness-50 w-full h-full object-cover absolute inset-0"
               />
               <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center">
-                <p className="text-white font-extrabold text-3xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-3xl text-center drop-shadow-lg px-2">
                   {" "}
                   Bois
                 </p>
-                <p className="text-body font-extrabold text-xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-xl text-center drop-shadow-lg px-2">
                   {" "}
                   23 concours
                 </p>
@@ -83,11 +83,11 @@ export default function DisplayCategories() {
                 className="rounded-2xl brightness-50 w-full h-full object-cover absolute inset-0"
               />
               <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center">
-                <p className="text-white font-extrabold text-3xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-3xl text-center drop-shadow-lg px-2">
                   {" "}
                   Terre
                 </p>
-                <p className="text-body font-extrabold text-xl text-center drop-shadow-lg px-2">
+                <p className="text-body dark:text-primary font-extrabold text-xl text-center drop-shadow-lg px-2">
                   {" "}
                   17 concours
                 </p>
@@ -104,11 +104,11 @@ export default function DisplayCategories() {
               className="rounded-2xl brightness-50 w-full h-full object-cover absolute inset-0"
             />
             <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center">
-              <p className="text-white font-extrabold text-3xl text-center drop-shadow-lg px-2">
+              <p className="text-body dark:text-primary font-extrabold text-3xl text-center drop-shadow-lg px-2">
                 {" "}
                 Multi
               </p>
-              <p className="text-body font-extrabold text-xl text-center drop-shadow-lg px-2">
+              <p className="text-body dark:text-primary font-extrabold text-xl text-center drop-shadow-lg px-2">
                 {" "}
                 3 concours
               </p>
